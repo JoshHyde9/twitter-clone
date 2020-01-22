93de07903a2a7b9a818768ebe710d88930ba969a
Josh Hyde <joshhyde@pop-os.localdomain>
2020-01-22 17:11:06 +1100

    Added cors

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,6 +1,9 @@
 const functions = require("firebase-functions");
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 const { db } = require("./util/admin");
 
