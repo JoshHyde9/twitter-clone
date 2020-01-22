054da449943411cf8de82f44cca6b311a9567387
Josh Hyde <joshhyde@pop-os.localdomain>
2020-01-22 17:49:33 +1100

    Added login page

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ const theme = createMuiTheme({
   palette: {
     primary: {
       light: "#33c9dc",
-      main: "#00bcd4",
+      main: "#01a0b5",
       dark: "#008394",
       contrastText: "#fff"
     },
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,174 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+import Logo from "../images/logo.png";
+
+// Material-UI
+import withStyles from "@material-ui/core/styles/withStyles";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+const styles = {
+  form: {
+    textAlign: "center"
+  },
+  image: {
+    maxWidth: "100px",
+    margin: "20px auto"
+  },
+  pageTitle: {
+    margin: "10px auto",
+    fontFamily: "Ubuntu"
+  },
+  textField: {
+    margin: "10px auto"
+  },
+  button: {
+    marginTop: 20,
+    position: "relative"
+  },
+  customError: {
+    color: "red",
+    fontSize: "0.8rem",
+    marginTop: 10
+  },
+  progress: {
+    position: "absolute"
+  },
+  route: {
+    color: "#008394"
+  }
+};
 
 export class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      loading: false,
+      errors: {}
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({
+      loading: true
+    });
+
+    const userData = {
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(userData)
+    })
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({
+          errors: data,
+          loading: false
+        });
+        if (!this.state.errors) {
+          this.props.history.push("/");
+        }
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
   render() {
-    return <h1>Login Page</h1>;
+    const { classes } = this.props;
+    const { errors, loading } = this.state;
+    return (
+      <Grid container className={classes.form}>
+        <Grid item sm />
+        <Grid item sm>
+          <img src={Logo} alt="Logo" className={classes.image} />
+          <Typography variant="h2" className={classes.pageTitle}>
+            Login
+          </Typography>
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              className={classes.textField}
+              helperText={errors.email}
+              error={errors.email ? true : false}
+              value={this.state.email}
+              onChange={this.handleChange}
+              fullWidth
+            ></TextField>
+            <TextField
+              id="password"
+              name="password"
+              type="password"
+              label="Password"
+              className={classes.textField}
+              helperText={errors.password}
+              error={errors.email ? true : false}
+              value={this.state.password}
+              onChange={this.handleChange}
+              fullWidth
+            ></TextField>
+            {errors.general && (
+              <Typography variant="body2" className={classes.customError}>
+                {errors.general}
+              </Typography>
+            )}
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              disabled={loading}
+            >
+              Login
+              {loading && (
+                <CircularProgress size={30} className={classes.progress} />
+              )}
+            </Button>
+            <br />
+            <small>
+              Don't have an account?{" "}
+              <Link to="/signup" className={classes.route}>
+                Sign Up
+              </Link>
+            </small>
+          </form>
+        </Grid>
+        <Grid item sm />
+      </Grid>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Login);
