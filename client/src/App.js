376eebd50152acf360c71165b7c2116b452b2057
Josh Hyde <joshhyde@pop-os.localdomain>
2020-01-22 20:28:37 +1100

    Added auth check

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
+import jwtDecode from "jwt-decode";
+
 import "./scss/style.css";
 import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
 import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
@@ -9,6 +11,7 @@ import themeFile from "./util/theme";
 
 // Components
 import NavBar from "./components/NavBar";
+import AuthRoute from "./util/AuthRoute";
 
 // Pages
 import Home from "./pages/Home";
@@ -16,6 +19,19 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
 const theme = createMuiTheme(themeFile);
+
+const token = localStorage.FBIdToken;
+
+let authenticated;
+if (token) {
+  const decodedToken = jwtDecode(token);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    window.location.href("/login");
+    authenticated = false;
+  } else {
+    authenticated = true;
+  }
+}
 
 export class App extends Component {
   render() {
@@ -25,8 +41,18 @@ export class App extends Component {
         <div className="container">
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
+            <AuthRoute
+              exact
+              path="/login"
+              component={Login}
+              authenticated={authenticated}
+            />
+            <AuthRoute
+              exact
+              path="/signup"
+              component={Signup}
+              authenticated={authenticated}
+            />
           </Switch>
         </div>
       </MuiThemeProvider>
